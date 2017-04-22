f0a9ae8d2930ff49e3758737e1d3c3fc9a1e494f
gmaker <mr.gmaker@yandex.ru>
2017-04-22 11:43:59 +0300

    first commit

---
+++ desktop/src/ru/znay/znay/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.znay.znay.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.znay.znay.LD38;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD38(), config);
+	}
+}
